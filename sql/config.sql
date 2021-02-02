a73f4d43046bf6af775745d9af61b1c22d0a5996
jan.vz94 <jan.vz94@gmail.com>
2021-02-02 19:30:50 +0100

    sql

---
+++ sql/config.sql
@@ -0,0 +1,3 @@
+create table user_account(ID int primary key AUTO_INCREMENT, LOGIN text, PASSWORD text);
+
+create table user_chords (ID int primary key AUTO_INCREMENT, NAME text, VALUE text, ID_USER int  references user_account(ID), PERCUSSION text, MELODY text );
